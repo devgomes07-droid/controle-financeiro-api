310aab97e7347bed433143b6adf326ae8ff573db
DevGomes <devgomes.07@gmail.com>
2026-05-18 20:44:43 -0300

    fix: add application.properties com profile prod

--- src/main/java/api_financeira/entities/User.java
+++ src/main/java/api_financeira/entities/User.java
@@ -29,7 +29,6 @@ public class User implements Serializable {
         this.email = email;
         this.password = password;
     }
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
